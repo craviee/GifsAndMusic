53e308418565c8f3f8339d3769dcf2a144a5067f
Craviee <danielcraviee@gmail.com>
2018-07-20 14:33:19 -0300

    working on #3 Issue

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function changeImg(value)
 {
     index += value;
-
+    player.loadVideoById(links[index]);
     if (index === 0)
     {
         back.disabled = true;
@@ -36,10 +36,52 @@ let links =
     "68ugkg9RePc"
 ];
 
+var tag = document.createElement('script');
+
+     tag.src = "https://www.youtube.com/iframe_api";
+     var firstScriptTag = document.getElementsByTagName('script')[0];
+     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+     // 3. This function creates an <iframe> (and YouTube player)
+     //    after the API code downloads.
+     var player;
+     function onYouTubeIframeAPIReady() {
+       player = new YT.Player('div2', {
+         height: '300',
+         width: '300',
+         videoId: 'o3hEYqIOfrU',
+         events: {
+           'onReady': onPlayerReady,
+           'onStateChange': onPlayerStateChange
+         }
+       });
+     }
+
+     // 4. The API will call this function when the video player is ready.
+     function onPlayerReady(event) {
+       event.target.playVideo();
+     }
+
+     // 5. The API calls this function when the player's state changes.
+     //    The function indicates that when playing a video (state=1),
+     //    the player should play for six seconds and then stop.
+     // var done = false;
+     function onPlayerStateChange(event) {
+       if (event.data == YT.PlayerState.PAUSED) {
+         // done = true;
+         body.backgroundImage = 'url(../img/' + index +'.png)';
+       }
+       else if (event.data == YT.PlayerState.PLAYING)
+       {
+         body.backgroundImage = 'url(../img/' + index +'.gif)';
+       }
+     }
+
+
 for(let i = 0; i < links.length; i++)
 {
-    links[i] = "https://www.youtube.com/embed/" + links[i] + "?autoplay=1&rel=0";
-    imgs.push(i + ".gif");
+    // links[i] = "https://www.youtube.com/embed/" + links[i] + "?enablejsapi=1&autoplay=1&rel=0";
+    // imgs.push(i + ".gif");
 }
 
 back.addEventListener('click', function(e) {changeImg(-1)});
